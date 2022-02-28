2d37fa46e631a6d7221c531274fd0290361b6202
Anca <A.Badiu@student.tudelft.nl>
2022-02-28 21:00:44 +0100

    Set up routes

--- server/server/app.js
+++ server/server/app.js
@@ -4,5 +4,10 @@ const http = require("http");
 const port = process.argv[2];
 const app = express();
 
+const indexRouter = require("./routes/index");
+
 app.use(express.static(__dirname + "/public"));
+
+app.get("/", indexRouter);
+
 http.createServer(app).listen(port);
